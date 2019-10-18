d74e89eaf038e26b01e393ae0325b6d3443f5bc0
Dmitriy Azarov <azarovdn@icloud.com>
2019-10-19 00:02:11 +1000

    Сделал все по уроку 3. Все работает

---
+++ ToDoFire/Controllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ToDoFire
+//
+//  Created by Дмитрий Азаров on 18.10.2019.
+//  Copyright © 2019 Dmitrii Azarov. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var warnLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func regisretTapped(_ sender: UIButton) {
+    }
+    
+}
+
